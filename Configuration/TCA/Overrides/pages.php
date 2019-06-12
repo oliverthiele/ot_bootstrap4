16661e19e1f2185324a8d55fcb4adbaf3554b602
Oliver Thiele <mail@oliver-thiele.de>
2019-06-12 21:29:03 +0200

    [TASK] Added Backend Layouts

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,2 +1,13 @@
 <?php
 defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'ot_bootstrap4',
+    'Configuration/PageTS/BackendLayouts.tsconfig',
+    'Backend Layouts: Homepage/Default'
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'ot_bootstrap4',
+    'Configuration/PageTS/AdditionalBackendLayouts.tsconfig',
+    'Additional Backend Layouts'
+);
