8976df21260694bfbb1b45f905af799e89261499
Oliver Thiele <mail@oliver-thiele.de>
2019-06-12 14:31:20 +0200

    [TASK] Added RTE Configuration with Bootstrap 4 Styles

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,3 +4,5 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['b'][] = 'OliverThiele\OtBootstrap4\ViewHelpers';
 
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['otBootstrap4'] = 'EXT:ot_bootstrap4/Configuration/RTE/OtBootstrap4.yaml';
+
