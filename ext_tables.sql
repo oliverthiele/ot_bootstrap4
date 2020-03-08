473e706ddfbd82e2c52be352f076d58468652e89
Oliver Thiele <mail@oliver-thiele.de>
2020-03-08 17:03:06 +0100

    [TASK] Add db field  disable_page_header

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,12 +1,16 @@
 CREATE TABLE tt_content (
-	bootstrap_col_xs int(11) DEFAULT '0' NOT NULL,
-	bootstrap_col_sm int(11) DEFAULT '0' NOT NULL,
-	bootstrap_col_md int(11) DEFAULT '0' NOT NULL,
-	bootstrap_col_lg int(11) DEFAULT '0' NOT NULL,
-	bootstrap_col_xl int(11) DEFAULT '0' NOT NULL,
+    bootstrap_col_xs int(11) DEFAULT '0' NOT NULL,
+    bootstrap_col_sm int(11) DEFAULT '0' NOT NULL,
+    bootstrap_col_md int(11) DEFAULT '0' NOT NULL,
+    bootstrap_col_lg int(11) DEFAULT '0' NOT NULL,
+    bootstrap_col_xl int(11) DEFAULT '0' NOT NULL,
 
-	bootstrap_css_outer varchar(255) NOT NULL DEFAULT '',
-	bootstrap_css_inner varchar(255) NOT NULL DEFAULT '',
+    bootstrap_css_outer varchar(255) NOT NULL DEFAULT '',
+    bootstrap_css_inner varchar(255) NOT NULL DEFAULT '',
 
-	disable_div_wrapper tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    disable_div_wrapper tinyint(1) unsigned DEFAULT '0' NOT NULL,
+);
+
+CREATE TABLE pages (
+    disable_page_header tinyint(1) unsigned DEFAULT '0' NOT NULL,
 );
