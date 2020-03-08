64a2c56def520c7fb016e7d9096ba142421eb848
Oliver Thiele <mail@oliver-thiele.de>
2020-03-08 12:27:11 +0100

    [BUGFIX] Correct the TSconfig include path

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -3,11 +3,11 @@ defined('TYPO3_MODE') or die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
     'ot_bootstrap4',
-    'Configuration/PageTS/BackendLayouts.tsconfig',
+    'Configuration/TSconfig/Page/BackendLayouts.tsconfig',
     'Backend Layouts: Homepage/Default'
 );
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
     'ot_bootstrap4',
-    'Configuration/PageTS/AdditionalBackendLayouts.tsconfig',
+    'Configuration/TSconfig/Page/AdditionalBackendLayouts.tsconfig',
     'Additional Backend Layouts'
 );
